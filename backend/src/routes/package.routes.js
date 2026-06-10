4053b0bc38c6f69cbf7c3f9566a302ee5db8639f
NeoSoftix <neosoftix@gmail.com>
2026-06-10 18:57:21 +0530

    error Fix

--- backend/src/routes/package.routes.js
+++ backend/src/routes/package.routes.js
@@ -1,29 +1,36 @@
-import express from "express"
-import { createPackage, deletePackage, getActivePackage, getAllPackage, getOnePackage, toggleStatus, updatePackage } from "../controllers/package.controller.js"
-import { verifyToken, allowedRoles } from "../middleware/auth.middleware.js"
+import express from "express";
+import {
+  createPackage,
+  deletePackage,
+  getActivePackage,
+  getAllPackage,
+  getOnePackage,
+  toggleStatus,
+  updatePackage,
+} from "../controllers/package.controller.js";
+import { verifyToken, allowedRoles } from "../middleware/auth.middleware.js";
 
-const router = express.Router()
+const router = express.Router();
 
-// create the package 
-router.post("/", verifyToken, allowedRoles("admin") ,createPackage)
+// create the package
+router.post("/", verifyToken, allowedRoles("admin"), createPackage);
 
 // get the active package
-router.get("/active", getActivePackage)
+router.get("/active", getActivePackage);
 
-// get all package 
-router.get("/", getAllPackage)
+// get all package
+router.get("/", getAllPackage);
 
-// get by id package 
-router.get("/:id", getOnePackage)
+// get by id package
+router.get("/:id", getOnePackage);
 
-// update the package 
-router.put("/:id",verifyToken, allowedRoles("admin"), updatePackage)
+// update the package
+router.put("/:id", verifyToken, allowedRoles("admin"), updatePackage);
 
-// toggle the package 
-router.patch("/:id", verifyToken, allowedRoles("admin"), toggleStatus)
+// toggle the package
+router.patch("/:id", verifyToken, allowedRoles("admin"), toggleStatus);
 
-// delete teh package 
-router.delete("/:id", verifyToken, allowedRoles("admin"), deletePackage)
+// delete teh package
+router.delete("/:id",verifyToken, allowedRoles("admin"), deletePackage); 
 
-
-export default router 
+export default router;
